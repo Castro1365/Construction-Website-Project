6356ce18b79d7c82502c551d93afd028544b71a3
Castro1365 <A00323594@mycambrian.ca>
2025-11-30 21:03:07 -0500

    script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-// Simple JS just for the contact form confirmation
 function handleContactSubmit(event) {
     event.preventDefault();
     alert("Thank you! Your request has been recorded. We will contact you soon.");
